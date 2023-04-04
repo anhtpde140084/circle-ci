a2f30730fe691e95744b3bb63c027c6844c3063b
phianh.dev <phianh.dev@gmail.com>
2023-04-05 00:13:23 +0700

    Update circle ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello my world, Thùy Vy yêu dấu
+         Hello my world, Thùy Vy yêu dấu .
         </p>
         <a
           className="App-link"
