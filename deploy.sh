ffe72d5c72a7715bb6039cfdd78fec182b862ecb
phianh.dev <phianh.dev@gmail.com>
2023-04-05 00:56:27 +0700

    Update circle ci

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cd `dirname $BASH_SOURCE`
 
-docker pull phianhdev/react-docker:lastest
 docker stop circle-ci-fe
 docker rm circle-ci-fe
-docker run -d --name circle-ci-fe phianhdev/react-docker:lastest
+docker pull phianhdev/react-docker:lastest
+docker-compose up -d
 
 cd - > /dev/null
