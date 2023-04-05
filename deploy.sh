8db81527918b29d594f9817e2131f8366c9279e0
phianh.dev <phianh.dev@gmail.com>
2023-04-05 23:05:06 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-cd `dirname $BASH_SOURCE`
-
-docker pull phianhdev/react-docker:lastest
-docker stop circle-ci-fe
-docker rm circle-ci-fe
-docker run -p 8088:80 circle-ci-fe phianhdev/react-docker:lastest
-cd - > /dev/null
+# docker pull phianhdev/react-docker:lastest
+# docker stop circle-ci-fe
+# docker rm circle-ci-fe
+# docker run -p 8088:80 circle-ci-fe phianhdev/react-docker:lastest
+#!/bin/bash
+docker-compose stop
+docker-compose rm -f
+docker-compose pull
+docker-compose up -d
