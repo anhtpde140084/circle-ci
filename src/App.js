d59cd045f8431766848c868deaa3f9ebd7e98690
phianh.dev <phianh.dev@gmail.com>
2023-04-07 00:11:34 +0700

    Update deploy.sh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello my world,kkk kk tui iu iu iuu em
+         Hello my world
         </p>
         <a
           className="App-link"
